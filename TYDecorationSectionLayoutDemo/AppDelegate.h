66df41395df203543c279d9ea24e3cf098aa0bf7
tany <122074809@qq.com>
2015-12-29 21:11:00 +0800

    Initial Commit

---
+++ TYDecorationSectionLayoutDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  TYDecorationSectionLayoutDemo
+//
+//  Created by tanyang on 15/12/29.
+//  Copyright © 2015年 tanyang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ TYDecorationSectionLayoutDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  TYDecorationSectionLayoutDemo
+//
+//  Created by tanyang on 15/12/29.
+//  Copyright © 2015年 tanyang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
